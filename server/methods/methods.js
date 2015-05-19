61a2ae45353c178509b8372841e0541f8f1593db
Joshua Bechard <josh@trashmountain.com>
2015-05-18 21:18:13 -0500

    Fix stored subscription data

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -287,7 +287,7 @@ Meteor.methods({
         } while(stripe_events.has_more);
 
         all_stripe_events.forEach(function (value){
-            if(value.data.object.object === 'customer' || value.data.object.object === 'card' || value.data.object.object === 'bank_account'){
+            if(value.data.type === 'customer.created'){
                 var request = value;
                 var event = Stripe_Events[request.type](request);
             }
