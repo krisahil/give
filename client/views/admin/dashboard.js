089fa2b8a51554fade81a7b26930f10a32df236d
Joshua Bechard <josh@trashmountain.com>
2015-05-26 12:09:24 -0500

    Fixed scheduled header image and moment deprecated function fix

--- client/views/admin/dashboard.js
+++ client/views/admin/dashboard.js
@@ -1,6 +1,6 @@
 Template.Dashboard.helpers({
     report_query: function () {
-        var startDate = moment().subtract('days', 29).format('YYYY-MM-DD');
+        var startDate = moment().subtract(29, 'days').format('YYYY-MM-DD');
         var endDate =  moment().format('YYYY-MM-DD');
         return '?startDate=' + startDate + '&endDate=' + endDate;
     }
