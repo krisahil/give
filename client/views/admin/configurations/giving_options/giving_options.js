e19bddf56ac84ab65b56b8b81a57e361f0ce734e
Joshua Bechard <josh@trashmountain.com>
2015-08-14 17:06:26 -0500

    [WIP] - Working on dropdown

--- client/views/admin/configurations/giving_options/giving_options.js
+++ client/views/admin/configurations/giving_options/giving_options.js
@@ -1,30 +1,162 @@
 function updateSelect(e, ui) {
-  console.log("received");
-  console.log(ui.item);
-  console.log(ui.item[0].id);
+  console.log("Updated");
 
-  var myTable = $( e.target );
-  var colArray = [];
-  var tableColumn = myTable.find('tr:not(:first) td:nth-child(2)');
-  console.log( $( tableColumn[0] ) );
+  $('#appendTablesHere table').each( function ( ) {
 
-  /*$('#testDropdown')
-    .find('option')
-    .remove();
+    console.log($(this));
 
-  _.forEach(tableColumn, function(value){
-    console.log($(value));
-    colArray.push($(value).text());
-    $('#testDropdown').append('<option value="' + $(value).text() + '">' + $(value).text() + '</option>');
-  });*/
+    var thisGroupCaptionID = $( this ).attr('id');
 
-  console.log( colArray );
+    console.log($(thisGroupCaptionID ).caption());
+
+    var thisOptGroup = $('#testDropdown').find('optgroup[data-id="' + thisGroupCaptionID + '"]' );
+
+    if(thisOptGroup){
+      console.log(thisOptGroup);
+
+      $('#testDropdown').find('optGroup[label="'+ thisOptGroup + '"' ).children().remove();
+
+      var colArray = [];
+      var tableColumn = $(this).find('tr:not(:first) td:nth-child(2)');
+      console.log( $( tableColumn[0] ) );
+      var nextGroupID = 'test';
+
+      _.forEach(tableColumn, function(value){
+        colArray.push($(value).text());
+        $('#testDropdown').append('<option value="' + $(value).text() + '">' + $(value).text() + '</option>');
+      });
+      $('#testDropdown').append('<optgroup data-id="' + nextGroupID + '' +
+        '" label="Group name (click me to edit)">');
+    }
+
+  });
+
+}
+
+
+function sortableFunction () {
+  $(".sortableTables").sortable({
+    items: 'tr:not(:first)',
+    cursor: 'move',
+    handle: '.glyphicon-move',
+    stack : '#set tr',
+    dropOnEmpty: true,
+    forceHelperSize: true,
+    forcePlaceholderSize: true,
+    connectWith: ".sortableTables",
+    update: function ( e, ui ) {
+      console.log( $(this).attr('id') );
+      if( $(this).attr('id') === "DTFundsTable" || $(this).attr('id') == undefined) {
+        console.log( $(this).attr('id') );
+        return;
+      } else {
+        console.log( $(this).attr('id') );
+        updateSelect (e, ui);
+      }
+    },
+    helper: function (e, li) {
+      this.copyHelper = li.clone().insertAfter(li);
+
+      $(this).data('copied', false);
+
+      return li.clone();
+    },
+    stop: function () {
+
+      var copied = $(this).data('copied');
+
+      if (!copied) {
+        this.copyHelper.remove();
+      }
+
+      this.copyHelper = null;
+    },
+    receive: function (e, ui) {
+      ui.sender.data('copied', true);
+      sortableIn = 1;
+    },
+    over: function(e, ui) {
+      sortableIn = 1;
+    },
+    out: function(e, ui) {
+      sortableIn = 0;
+    },
+    beforeStop: function(e, ui) {
+      if (sortableIn === 0 && $(this).attr('id') !== "DTFundsTable") {
+        ui.item.remove();
+      }
+    }
+
+  });
 }
 
 /*****************************************************************************/
 /* GivingOptions: Event Handlers */
 /*****************************************************************************/
 Template.GivingOptions.events({
+  'click #addGroupButton': function () {
+    var lastTableID, nextID, lastGroupID, nextGroupID;
+
+    console.log( "Got to addGroupButton Click" );
+    lastTableID = $( '#appendTablesHere table:last' ).attr( 'id' );
+    lastGroupID = $( '#appendTablesHere table:last caption' ).attr( 'id' );
+    nextID = lastTableID .replace(/(\d+)+/g, function(match, number) {
+      return parseInt(number)+1;
+    });
+
+    nextGroupID = lastGroupID .replace(/(\d+)+/g, function(match, number) {
+      return parseInt(number)+1;
+    });
+
+
+    console.log(nextID);
+
+    var content="";
+    content += '<section>';
+    content += '<table id="' + nextID + '" class="rwd-table sortableTables">';
+    content += '<caption contenteditable class="groupName" id="' + nextGroupID +'"';
+    content += "groupCaption1\">Group name (click me to edit)<\/caption>";
+    content += "<tr>";
+    content += "<th><span class=\"glyphicon glyphicon-minus-sign\" aria-hidden=\"true\"><\/span> Delete";
+    content += "<\/th>";
+    content += "<th>Fund Name<\/th>";
+    content += "<th>Donor Tools ID<\/th>";
+    content += "<\/tr>";
+    content += "<tr id=\"63661\" data-name=\"Where Most Needed\">";
+    content += "<td><span class=\"glyphicon glyphicon-move\" aria-hidden=\"true\"><\/span><\/td>";
+    content += "<td class=\"table-editable-content\" contenteditable data-th=\"Name\">Where Most Needed<\/td>";
+    content += "<td class=\"table-editable-content\" contenteditable data-th=\"Donor Tools ID\">63661<\/td>";
+    content += "<\/tr>";
+    content += "<\/table>";
+    content += "<\/section>";
+
+    $( '#appendTablesHere' ).append( content );
+    sortableFunction ();
+
+    $('#testDropdown').append('<optgroup data-id="' + nextGroupID + '' +
+      '" label="Group name (click me to edit)">\n<option>Where Most Needed</option>\n</optgroup>');
+  },
+  'blur .groupName': function ( e ) {
+    console.log($( e.target ).text());
+    var thisGroupID = $( e.target ).attr('id');
+    var newLabel = $( e.target ).text();
+
+
+    $('#testDropdown').find('optgroup[data-id="' + thisGroupID + '"]' ).attr('label', newLabel);
+    $('#testDropdown').change();
+  },
+  'click .groupName': function ( e ) {
+
+    var thisGroupID = $( e.target ).attr('id');
+
+    if($( e.target ).text() === 'Group name (click me to edit)'){
+      $( e.target ).text('');
+    }
+    else {
+    }
+
+
+  }
 });
 
 /*****************************************************************************/
@@ -53,26 +185,10 @@ Template.GivingOptions.onCreated(function () {
 
 Template.GivingOptions.onRendered(function () {
 
-  // TODO: make sure that the select element doesn't pull in the changes when
-  // an item is moved into the DT_funds list
-  $(".sortableTables").sortable({
-    items: 'tr:not(:first)',
-    cursor: 'move',
-    handle: '.glyphicon-move',
-    stack : '#set tr',
-    dropOnEmpty: true,
-    forceHelperSize: true,
-    forcePlaceholderSize: true,
-    connectWith: ".sortableTables",
-    /*receive: function ( e, ui ) {
-      updateSelect (e, ui);
-    },*/
-    update: function ( e, ui ) {
-      updateSelect (e, ui);
-    }
-  });
+  // Start the function to setup the table connections and make them sortable
+  sortableFunction ();
 
-  $('#testDropdown').select2({dropdownCssClass: 'dropdown-inverse', open: true});
+  $('#testDropdown').select2({dropdownCssClass: 'dropdown-inverse'});
 
 });
 
