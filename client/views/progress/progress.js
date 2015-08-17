255cb0c64b1d2bede24f1161c5202b3b647b7ef1
Joshua Bechard <josh@trashmountain.com>
2015-08-17 15:29:46 -0500

    Changes to progress

--- client/views/progress/progress.js
+++ client/views/progress/progress.js
@@ -5,23 +5,25 @@ Template.Progress.helpers({
 });
 
 Template.Progress.onRendered(function () {
-  var progressbar = $('progress'),
-    max = progressbar.attr('max'),
-    time = (1000/max)*60,
-    value = progressbar.val();
+  if(Session.get('params.note')) {
+    var progressbar = $('progress'),
+      max = progressbar.attr('max'),
+      time = (500/max)*5,
+      value = progressbar.val();
 
-  var loading = function() {
-    value += 1;
-    addValue = progressbar.val(value);
+    var loading = function() {
+      value += 1;
+      addValue = progressbar.val(value);
 
-    $('.progress-value').html(value + ' Children Sponsored');
+      $('.progress-value').html(value + ' Children Sponsored');
 
-    if (value === 400) {
-      clearInterval(animate);
-    }
-  };
+      if (value === 350) {
+        clearInterval(animate);
+      }
+    };
 
-  var animate = setInterval(function() {
-    loading();
-  }, time);
+    var animate = setInterval(function() {
+      loading();
+    }, time);
+  }
 });
