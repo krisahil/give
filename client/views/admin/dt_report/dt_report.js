9a86fec63ba04ad778c338636fe857ea95436e34
Joshua Bechard <josh@trashmountain.com>
2015-10-26 10:33:06 -0500

    Changes

--- client/views/admin/dt_report/dt_report.js
+++ client/views/admin/dt_report/dt_report.js
@@ -11,18 +11,6 @@ Template.DtReport.events({
       if(!error) {
         getDataButton.button("reset");
 
-        // output this to the site
-        // put this in a place where a javascript get request could pull in this number
-        // from Squarespace
-
-        //TODO: two functions should exist for updating this number
-        //first: one for updating short term.
-        //this function should store a date of last "quick-data-pull"
-        //and then only pull data since that pull
-        //or just process the split each time a gift is given (to CS), make it part of that flow
-        //second: pull 30 days of history, in case Gayla makes a change to a previous donation
-
-
         console.dir(result);
         Bert.alert("Got the DT split Data successfully.", "success");
       } else {
@@ -43,43 +31,12 @@ Template.DtReport.events({
 
         console.dir(result);
 
-        Bert.alert("Got the DT split Data successfully.", "success");
+        Bert.alert("Got the DT split Data successfully. Value is: " + result, "success");
       } else {
         showDataButton.button("reset");
         console.dir(error);
         Bert.alert("That didn't work...", "danger");
       }
     });
-  }
-});
-
-/*****************************************************************************/
-/* DtReport: Helpers */
-/*****************************************************************************/
-Template.DtReport.helpers({
-  memoType: function () {
-    //TODO: how to spit out the different memo types here
-    //return DT_splits.find({memo: 'annual'});
-  },
-  totalFunds: function ( ) {
-
-  },
-  groupResult: function () {
-
-  },
-  kidsServed: function () {
-
-  },
-  totalOfGroups: function () {
-    /*var totalAmount = 0.0;
-    $("#splitsTable td.kidsServed").each ( function(){
-      var currentAmount = parseFloat ( $(this).text());
-      // if your td text contains $ symbol at the beginning you can do like this
-      //var currentAmount = parseFloat ( $(this).text().substring(1));
-      totalAmount += currentAmount;
-    });
-
-    return totalAmount;*/
-
   }
 });
