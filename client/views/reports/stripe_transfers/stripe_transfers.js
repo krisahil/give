cb400b1b93de225e9a817a8ee1434f4f453eb030
Joshua Bechard <josh@trashmountain.com>
2015-10-30 12:49:39 -0500

    Added Kadira cpu profiler

--- client/views/reports/stripe_transfers/stripe_transfers.js
+++ client/views/reports/stripe_transfers/stripe_transfers.js
@@ -57,6 +57,27 @@ Template.StripeTransfers.onRendered(function () {
       $('#ui-datepicker-div').addClass("no-calendar");
     }
   });
+
+  var dynamicStyle = $("<style> .ui-datepicker-calendar { display: none; } </style>")
+    .attr("id", "dynamicDatepickerStyle");
+  $("#monthDate1").datepicker({
+    beforeShow: function ()
+                {
+                  $("body").append(dynamicStyle);
+                },
+
+    onClose: function ()
+             {
+               $("#dynamicDatepickerStyle").remove();
+             }
+  });
+
+  $("#ui-datepicker-div").delegate(".ui-datepicker-close", 'click', function(){
+    var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+    var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+    $("#monthDate1").datepicker('option', 'defaultDate', new Date(year, month, 1));
+    $("#monthDate1").datepicker('setDate', new Date(year, month, 1));
+  });
 });
 
 Template.StripeTransfers.onDestroyed(function () {
